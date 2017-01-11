e5f3b410696c329f6cf54699772ad576f13d157b
xwh123807 <xwh123807@gmail.com>
2017-01-11 23:20:04 +0800

    add fly code

---
+++ org.easycloud.platform.metadata/src/main/java/org/easycloud/platform/metadata/MetaDataApplication.java
@@ -0,0 +1,11 @@
+package org.easycloud.platform.metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaDataApplication {
+	public static void main(String args[]){
+		SpringApplication.run(MetaDataApplication.class, args);
+	}
+}
