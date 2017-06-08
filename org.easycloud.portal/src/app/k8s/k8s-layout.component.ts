72b10507cb85e2d8f04ac3f0cbade6ff86a3bbc8
xwh123807 <xwh123807@gmail.com>
2017-06-09 00:46:46 +0800

    增加默认导航

--- org.easycloud.portal/src/app/k8s/k8s-layout.component.ts
+++ org.easycloud.portal/src/app/k8s/k8s-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-k8s-layout',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class K8sLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.router.navigate(['/k8s/nodes']);
   }
 
 }
